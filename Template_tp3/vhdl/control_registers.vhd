0af0f0052381a57b933c68b25285952ba757806b
Marco Voegeli <alvin.mvoegeli@gmail.com>
2020-03-06 11:18:23 +0100

    test works

--- Template_tp3/vhdl/control_registers.vhd
+++ Template_tp3/vhdl/control_registers.vhd
@@ -24,16 +24,15 @@ signal s_rddata, s_ctl0, s_ctl1, s_ctl3, s_ctl4, s_ctl5: std_logic_vector(31 dow
 
 begin
 
-main: process(clk, reset_n)
+main: process(clk, reset_n, address, wrdata, s_ctl0, write_n, backup_n, restore_n )
 begin
     if (reset_n = '0') then 
             s_rddata <= (31 downto 0 => '0');
             s_ctl0 <= (31 downto 0 => '0'); 
             s_ctl1 <= (31 downto 0 => '0');
             s_ctl3 <= (31 downto 0 => '0');
-            s_ctl4 <= (31 downto 0 => '0');
             s_ctl5 <= (31 downto 0 => '0');
-            ipending <= '0';
+            s_rddata <= (31 downto 0 => 'Z');
     else
         if(rising_edge(clk)) then
             if(write_n = '0') then 
@@ -41,7 +40,6 @@ begin
                     when "000" => s_ctl0 <= wrdata;
                     when "001" => s_ctl1 <= wrdata;
                     when "011" => s_ctl3 <= wrdata;
-                    when "100" => s_ctl4 <= wrdata;
                     when "101" => s_ctl5 <= wrdata;
                     when others =>
                 end case;
@@ -54,16 +52,23 @@ begin
                 s_ctl0(0) <= s_ctl1(0);
             end if;
         end if;
-        case address is
-            when "000" => s_rddata <= s_ctl0;
-            when "001" => s_rddata <= s_ctl1;
-            when "011" => s_rddata <= s_ctl3;
-            when "100" => s_rddata <= s_ctl4;
-            when "101" => s_rddata <= s_ctl5;
-            when others =>
-        end case;
     end if;   
-end process main; 
+end process main;
 
+asynchronous: process(address, s_ctl0, s_ctl1, s_ctl3, s_ctl4, s_ctl5)
+begin
+    
+    case address is
+        when "000" => s_rddata <= s_ctl0;
+        when "001" => s_rddata <= s_ctl1;
+        when "011" => s_rddata <= s_ctl3;
+        when "100" => s_rddata <= s_ctl4;
+        when "101" => s_rddata <= s_ctl5;
+        when others =>
+    end case;
+end process asynchronous;
+
+s_ctl4 <= irq and s_ctl3;
+rddata <= s_rddata;
 ipending <= '1' when s_ctl0(0) = '1' and (s_ctl4 /= (31 downto 0 => '0')) else '0';
 end synth;
