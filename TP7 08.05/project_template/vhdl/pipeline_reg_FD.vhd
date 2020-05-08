d3ab6437f450f3712265a8afe41f6b17c561c817
Roy shoham <roy.shoham95@gmail.com>
2020-05-08 10:17:58 +0200

    :)

--- TP7 08.05/project_template/vhdl/pipeline_reg_FD.vhd
+++ TP7 08.05/project_template/vhdl/pipeline_reg_FD.vhd
@@ -15,7 +15,22 @@ entity pipeline_reg_FD is
 end pipeline_reg_FD;
 
 architecture synth of pipeline_reg_FD is
+    signal I_rddata_out_s : std_logic_vector(31 downto 0);
+    signal next_addr_out_s : std_logic_vector(15 downto 0);
 
 begin
-   
+main: process(reset_n, clk)
+begin
+    if (reset_n = '0') then
+        I_rddata_out_s <= (31 downto 0 => '0');
+        next_addr_out_s <= (15 downto 0 => '0');
+    elsif(rising_edge(clk)) then
+        I_rddata_out_s <= I_rddata_in;
+        next_addr_out_s <= next_addr_in;
+    end if;
+end process main;
+
+I_rddata_out <= I_rddata_out_s;
+next_addr_out <= next_addr_out_s;
+
 end synth;
